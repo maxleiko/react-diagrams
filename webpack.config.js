35df78d0b68290b7eebd536b240ea1f806eda90d
maxleiko <max.tricoire@gmail.com>
2018-03-10 11:42:10 +0100

    fix #184

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var webpack = require("webpack");
 var plugins = [];
 
-//do we minify it all
-if(process.env.NODE_ENV === 'production'){
-	console.log("creating production build");
+// do we minify it all
+if(process.env.NODE_ENV === 'production') {
+	console.log('creating production build');
 	plugins.push(new webpack.optimize.UglifyJsPlugin({
 		mangle: {
 			keep_fnames: true
@@ -11,7 +11,8 @@ if(process.env.NODE_ENV === 'production'){
 		compress: {
 			keep_fnames: true,
 			warnings: false,
-		}
+		},
+		sourceMap: true,
 	}));
 	plugins.push(new webpack.DefinePlugin({
 		'process.env.NODE_ENV': 'production',
@@ -21,53 +22,51 @@ if(process.env.NODE_ENV === 'production'){
 /**
  * @author Dylan Vorster
  */
-module.exports =
-	//for building the umd distribution
-	{
-		entry: './src/main.ts',
-		output: {
-			filename: 'main.js',
-			path: __dirname + '/dist',
-			libraryTarget: 'umd',
-			library: 'storm-react-diagrams'
+module.exports = {
+	entry: './src/main.ts',
+	output: {
+		filename: 'main.js',
+		path: __dirname + '/dist',
+		libraryTarget: 'umd',
+		library: 'storm-react-diagrams'
+	},
+	externals: {
+		react: {
+			root: 'React',
+			commonjs2: 'react',
+			commonjs: 'react',
+			amd: 'react'
 		},
-		externals: {
-			react: {
-				root: 'React',
-				commonjs2: 'react',
-				commonjs: 'react',
-				amd: 'react'
+		'react-dom': {
+			root: 'ReactDOM',
+			commonjs2: 'react-dom',
+			commonjs: 'react-dom',
+			amd: 'react-dom'
+		},
+		"lodash": {
+			commonjs: 'lodash',
+			commonjs2: 'lodash',
+			amd: '_',
+			root: '_'
+		}
+	},
+	plugins:plugins,
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader'
 			},
-			'react-dom': {
-				root: 'ReactDOM',
-				commonjs2: 'react-dom',
-				commonjs: 'react-dom',
-				amd: 'react-dom'
-			},
-			"lodash": {
-				commonjs: 'lodash',
-				commonjs2: 'lodash',
-				amd: '_',
-				root: '_'
+			{
+				enforce: "pre",
+				test: /\.js$/,
+				loader: "source-map-loader"
 			}
-		},
-		plugins:plugins,
-		module: {
-			rules: [
-				{
-					enforce: 'pre',
-					test: /\.js$/,
-					loader: "source-map-loader"
-				},
-				{
-					test: /\.tsx?$/,
-					loader: 'ts-loader'
-				}
-			]
-		},
-		resolve: {
-			extensions: [".tsx", ".ts", ".js"]
-		},
-		devtool: process.env.NODE_ENV === 'production'?false:'eval-cheap-module-source-map'
-	}
-;
+		]
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"]
+	},
+	// Enable sourcemaps for debugging webpack's output.
+	devtool: "source-map",
+};
